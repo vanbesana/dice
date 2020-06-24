2b2d6309aefcea2d374d2cd77d7c8bb391241c60
Jhovanie Besana <45024433+vanbesana@users.noreply.github.com>
2020-06-24 22:35:43 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+
+
+
+var randomNo1 = Math.floor(Math.random() * 6) + 1; //random 1-6
+
+var randomDiceImage = "dice" + randomNo1 + ".png"; // dice1.png-6
+
+var randomImageSource = "images/" + randomDiceImage; //image source dice 1-6
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+
+var randomNo2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "images/dice" + randomNo2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+
+if (randomNo1 > randomNo2){
+	document.querySelector("h1").textContent = "Player 1 Wins!";
+}
+else if (randomNo1 == randomNo2){
+	document.querySelector("h1").textContent = "Draw!";
+}
+else{
+	document.querySelector("h1").textContent = "Player 2 Wins!";
+}
+
+
+
+// console.log(player1, player2);
+
+//update dice
+// document.getElementById('player1').src = 'images/dice' + player1 +'.png';
+// document.querySelector('#player2').src = 'images/dice' + player2 +'.png';
+
