37673aa35d426e3b7aa8f8e2e761809d8a8f6323
Jhovanie Besana <45024433+vanbesana@users.noreply.github.com>
2020-06-29 21:48:08 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -20,13 +20,13 @@ document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 
 if (randomNo1 > randomNo2){
-	document.querySelector("h1").textContent = "Player 1 Wins!";
+	document.querySelector("h1").innerHTML = "Player 1 Wins!";
 }
 else if (randomNo1 == randomNo2){
-	document.querySelector("h1").textContent = "Draw!";
+	document.querySelector("h1").innerHTML = "Draw!";
 }
 else{
-	document.querySelector("h1").textContent = "Player 2 Wins!";
+	document.querySelector("h1").innerHTML = "Player 2 Wins!";
 }
 
 
